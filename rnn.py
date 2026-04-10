781f37579d11ce0774d1924cbb6181851f02337a
arpanmondal <arpanmondal455@gmail.com>
2026-04-10 10:25:54 +0530

    initial code

---
+++ rnn.py
@@ -0,0 +1,138 @@
+import pandas as pd
+df = pd.read_csv('100_Unique_QA_Dataset.csv')
+df.head()
+
+#tokenize
+def tokenize(text):
+  text = text.lower()
+  text = text.replace('?', '')
+  text = text.replace("'", "")
+  return text.split()
+
+#vocab
+vocab = {'<UNK>' : 0}
+
+def build_vocab(row):
+  tokenized_question = tokenize(row['question'])
+  tokenized_answer = tokenize(row['answer'])
+  merged_tokens = tokenized_question + tokenized_answer
+  print(merged_tokens)
+
+  for token in merged_tokens:
+    if token not in vocab:
+      vocab[token] = len(vocab)
+
+df.apply(build_vocab, axis=1)
+
+print(vocab)
+
+print(len(vocab))
+
+
+# convert word to numerical indices
+def text_to_indices(text, vocab):
+    index_text = []
+
+    for token in tokenize(text):
+        if token in vocab:
+            index_text.append(vocab[token])
+        else:
+            index_text.append(vocab['<UNK>'])
+
+    return index_text
+
+print(text_to_indices("What is my name", vocab))
+
+import torch
+from torch.utils.data import DataLoader, Dataset
+
+
+class QADataset(Dataset):
+    def __init__(self, df, vocab):
+        self.df = df
+        self.vocab = vocab
+
+    def __len__(self):
+        return self.df.shape[0]
+
+    def __getitem__(self, index):
+        numerical_question = text_to_indices(self.df.iloc[index]['question'], self.vocab)
+        numerical_answer = text_to_indices(self.df.iloc[index]['answer'], self.vocab)
+
+        return torch.tensor(numerical_question), torch.tensor(numerical_answer)
+
+dataset = QADataset(df, vocab)
+dataloader = DataLoader(dataset, batch_size=1, shuffle=True)
+
+for question, answer in dataloader:
+  print(question, answer[0])
+
+import torch.nn as nn
+
+class SimpleRNN(nn.Module):
+
+  def __init__(self, vocab_size):
+    super().__init__()
+    self.embedding = nn.Embedding(vocab_size, embedding_dim=50)
+    self.rnn = nn.RNN(50, 64, batch_first=True)
+    self.fc = nn.Linear(64, vocab_size)
+
+  def forward(self, question):
+    embedded_question = self.embedding(question)
+    hidden, final = self.rnn(embedded_question)
+    output = self.fc(final.squeeze(0))
+
+    return output
+
+learning_rate = 0.001
+epochs = 20
+
+model = SimpleRNN(len(vocab))
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+
+# training loop
+for epoch in range(epochs):
+    total_loss = 0
+
+    for question, answer in dataloader:
+        optimizer.zero_grad()
+
+        # forward pass
+        output = model(question)
+
+        # loss
+        loss = criterion(output, answer[0])
+
+        # gradients
+        loss.backward()
+
+        # update
+        optimizer.step()
+
+        total_loss = total_loss + loss.item()
+
+    print(f"Epoch : {epoch + 1}, Loss : {total_loss:4f}")
+
+def predict(model, question, threshold = 0.5):
+
+  #convert question to numbers
+  numerical_question = text_to_indices(question, vocab)
+
+  #tensor
+  question_tensor = torch.tensor(numerical_question).unsqueeze(0)
+
+  #send to model
+  output = model(question_tensor)
+
+  #convert logits to probability
+  probs = torch.nn.functional.softmax(output, dim = 1)
+
+  #find index of max probability
+  value, index = torch.max(probs, dim = 1)
+
+  if value < threshold:
+    print("I don't know")
+  else:
+    print(list(vocab.keys())[index])
